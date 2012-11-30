6a2b26425c12f64573933500e6879cefc62e9539
Zackneifein <zackneifein@gmail.com>
2012-11-30 09:07:31 +0100

    Mise à jour des vues

--- BooXtore-war/src/com/compte/web/CompteMBean.java
+++ BooXtore-war/src/com/compte/web/CompteMBean.java
@@ -1,5 +1,6 @@
 package com.compte.web;
 
+import Jpa.Classes.Client;
 import Ejb.CompteEJBRemote;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -17,14 +18,88 @@ public class CompteMBean
     @EJB
     private CompteEJBRemote CompteEJB;
 
+    private String login;
+    private String pwd;
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String adr;
+    private String codePostal;
+    private String ville;
+
     /** Creates a new instance of CompteMBean */
     public CompteMBean()
     {
 
     }
 
-    public void inscription(String login, String mdp, String nom, String prenom, String mail, String adr, String codePostal, String ville){
-        CompteEJB.inscription(login, mdp, nom, prenom, mail, adr, codePostal, ville);
+    public String getLogin()
+    {
+        return login;
+    }
+    public void setLogin(String login)
+    {
+        this.login = login;
+    }
+    public String getPwd()
+    {
+        return pwd;
+    }
+    public void setPwd(String pwd)
+    {
+        this.pwd = pwd;
+    }
+    public String getNom()
+    {
+        return nom;
+    }
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+    public String getPrenom()
+    {
+        return prenom;
+    }
+    public void setPrenom(String prenom)
+    {
+        this.prenom = prenom;
+    }
+    public String getMail()
+    {
+        return mail;
+    }
+    public void setMail(String mail)
+    {
+        this.mail = mail;
+    }
+    public String getAdr()
+    {
+        return adr;
+    }
+    public void setAdr(String adr)
+    {
+        this.adr = adr;
+    }
+    public String getCodePostal()
+    {
+        return codePostal;
+    }
+    public void setCodePostal(String codePostal)
+    {
+        this.codePostal = codePostal;
+    }
+    public String getVille()
+    {
+        return ville;
+    }
+    public void setVille(String ville)
+    {
+        this.ville = ville;
+    }
+
+    public void inscription(){
+        CompteEJB.inscription(login, adr, nom, prenom, mail, adr, codePostal, ville);
     }
 
 }
